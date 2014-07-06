fee81d27d5d04fa95fd92c34336255d6fcc34f9e
Steve Schofield <stevebschofield@gmail.com>
2014-07-06 23:09:01 +0100

    added uiscrollview and handling for device orientation changes

---
+++ WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Calcohol
+//
+//  Created by Steven Schofield on 03/07/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhiskeyViewController : UIViewController
+
+@end
